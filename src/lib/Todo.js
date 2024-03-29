fe0aa3cc2f58049e8daa9144734dabe7ff941594
Your Name <knnymrls@outlook.com>
2024-03-28 19:44:04 -0500

    Go a lot of the main functionality for the todos

--- src/lib/Todo.js
+++ src/lib/Todo.js
@@ -2,11 +2,13 @@ export class Todo {
     /**
      * Creates a new Todo instance
      * @param {String} title
+     * @param {Number} id
      * @param {String} description'
      * @param {Date} dueDate 
      */
-    constructor(title, description, dueDate) {
+    constructor(title, id, description, dueDate) {
         this.title = title;
+        this.id = id;
         this.description = description;
         this.dueDate = dueDate;
         this.done = false;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,63 @@
 <script>
 	import { Todo } from '$lib/Todo';
 
-	let todo = new Todo('Kenny', 'Do the dishes', new Date(2023, 9, 13));
+	let title = '';
+	let desciption = '';
+
+	/**
+	 * @type {Todo[]} todos
+	 */
+	let todos = [];
+
+	/**
+	 * Adds a new todo to the list of todos
+	 * @param {Todo} todo The todo to add
+	 */
+	function addTodo(todo) {
+		todos = [...todos, todo];
+	}
+
+	/**
+	 * Removes a todo from the list of todos
+	 * @param {number} id The id of the todo to remove
+	 */
+	function removeTodo(id) {
+		todos = todos.filter((todo) => id !== todo.id);
+	}
+
+	/**
+	 * Gets the new id for a todo
+	 * @returns {number} The new id
+	 */
+	function getNewId() {
+		// Reindexes the todos
+		todos = todos.map((todo) => ({ ...todo, toggle: () => {} }));
+
+		if (todos.length === 0) return 1;
+		// The new index is one more than the highest current index
+		return todos[todos.length - 1].id + 1;
+	}
+
+	/**
+	 * Handles the form submission
+	 */
+	function handleSubmit() {
+		let newTodo = new Todo(title, getNewId(), desciption, new Date());
+		addTodo(newTodo);
+	}
 </script>
 
-<h1>{todo.title} {todo.description} {todo.dueDate}</h1>
+<main>
+	<input type="text" bind:value={title} placeholder="Name" />
+	<input type="text" bind:value={desciption} placeholder="Name" />
+	<button on:click={handleSubmit}>Add</button>
+
+	{#each todos as todo (todo.id)}
+		<div>
+			<h2>{todo.title}</h2>
+			<p>{todo.description}</p>
+			<p>{todo.dueDate}</p>
+			<button on:click={() => removeTodo(todo.id)}>Remove</button>
+		</div>
+	{/each}
+</main>
